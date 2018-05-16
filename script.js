7ecf6e20a6e07247bde8dae6e663c0b344e92252
Emilie Houlby <emiliehoulby@gmail.com>
2018-05-17 00:02:55 +0200

    sider

--- script.js
+++ script.js
@@ -76,3 +76,26 @@ function initMap() {
             window.location.href = ('omvroegumsider/ballonpark.html')
           }
       });
+
+
+
+
+      var slideIndex = 0;
+      showSlides();
+
+      function showSlides() {
+          var i;
+          var slides = document.getElementsByClassName("billede");
+          var dots = document.getElementsByClassName("dot");
+          for (i = 0; i < slides.length; i++) {
+             slides[i].style.display = "none";
+          }
+          slideIndex++;
+          if (slideIndex > slides.length) {slideIndex = 1}
+          for (i = 0; i < dots.length; i++) {
+              dots[i].className = dots[i].className.replace(" active", "");
+          }
+          slides[slideIndex-1].style.display = "block";
+          dots[slideIndex-1].className += " active";
+          setTimeout(showSlides, 2000); s
+      }
