65c65ec81f4913e5ef8f4241ea1a86e9a415775d
Emilie Houlby <emiliehoulby@gmail.com>
2018-05-17 09:32:59 +0200

    hurtig ændring i java

--- script.js
+++ script.js
@@ -76,7 +76,36 @@ function initMap() {
             window.location.href = ('omvroegumsider/ballonpark.html')
           }
       });
-/*----------------------------- SLIDESHOW BAllONPARK -------------------------------------*/
+
+
+
+
+
+      var slideIndex = 0;
+      showSlides();
+
+      function showSlides() {
+          var i;
+          var slides = document.getElementsByClassName("billede");
+          var dots = document.getElementsByClassName("dot");
+          for (i = 0; i < slides.length; i++) {
+             slides[i].style.display = "none";
+          }
+          slideIndex++;
+          if (slideIndex > slides.length) {slideIndex = 1}
+          for (i = 0; i < dots.length; i++) {
+              dots[i].className = dots[i].className.replace(" active", "");
+          }
+          slides[slideIndex-1].style.display = "block";
+          dots[slideIndex-1].className += " active";
+          setTimeout(showSlides, 2000); s
+      }
+
+
+
+
+
+
       var imageSlides = document.querySelectorAll('.imageslides');
       var leftArrow = document.querySelector('.arrow-left');
       var rightArrow = document.querySelector('.arrow-right');
@@ -124,5 +153,3 @@ function initMap() {
         });
 
     startSlide();
-
-/*----------------------------- SLIDESHOW SKOV -------------------------------------*/
